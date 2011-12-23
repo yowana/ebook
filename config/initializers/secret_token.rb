c983e247a253a2a95c2484566f9ed3828129a2b3
yowana <y.kuvaka.09@aberdeen.ac.uk>
2011-12-23 12:20:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebook::Application.config.secret_token = '81d9897ebb59b0c9437ee88c03ef40ac52b48a8b425de08f7f908076ed7468586aba43dba6efd78b4035cd933566935071b122d63ef80f59e3bf2f8d910b832c'
